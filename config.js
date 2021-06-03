c2a3745585a1af8fbdb7eef4f4795bef57ed5cf3
Anahista <85278292+Anahista@users.noreply.github.com>
2021-06-03 01:18:06 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,25 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Gửi Mỹ Vân siêu xinh đẹp siêu cấp vũ trụ :3',
+    introDesc: `Mưa đan ngàn sợi bên đời
+Mưa không nhắn đủ một lời yêu em
+Nắng về cỏ lá đua chen
+Có làm ấm lại tình quên thuở nào
+Gió ơi cứ vẫn xạc xào
+Tim ngây thơ lắm thể nào cũng rung
+Đêm che một nỗi ngại ngùng
+Bâng quơ anh hỏi có cùng đường mơ
+Tình anh quấn quýt trong thơ
+Này em thế đã ngẩn ngơ động lòng?`,
+    btnIntro: '<3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Yêu anh <33',
+    btnNo: 'Không :)',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'chiều nay 6h anh qua đón em nha công chúa. :3',
+    btnAccept: 'Dạaaaaaaaaa <3',
+    messLink: 'https://m.me/Thanhhau.MyVan.01234' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
